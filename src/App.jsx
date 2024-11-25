db6bfed307d482260701aab8c5d985fe8b1bc5a7
arahman225 <upworkabdurrahman@gmail.com>
2024-11-25 08:20:50 +0600

    second

--- src/App.jsx
+++ src/App.jsx
@@ -4,46 +4,49 @@ import './App.css'
 
 function App() {
 
-const [users, setUsers] = useState([]);
-useEffect(()=>{
-  fetch('http://localhost:5000/users')
-  .then(res =>res.json())
-  .then(data =>setUsers(data))
-}, [])
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
 
-const hnadleSubmit = e =>{
-  e.preventDefault();
-  const form = e.target;
-  const name = form.name.value;
-  const email = form.email.value;
+  const hnadleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user)
 
-  const user = {name, email}
-  console.log(user)
+    fetch('http://localhost:5000/users',{
+      method: "POST",
+      headers:{
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res =>res.json())
+    .then(data =>{
+      console.log(data)
 
+      const newUsers = [...users, data];
+      setUsers(newUsers)
 
-  fetch('http://localhost:5000/users',{
-    method:"POST",
-    headers:{
-      "Content-type": "application/json"
-    },
-    body:JSON.stringify(user)
-  })
-  .then(res =>res.json())
-  .then(data =>{
-    console.log(data)
-  })
+      form.reset()
+    })
+  }
 
-}
   return (
     <>
 
       <h1>Users here {users.length} </h1>
       <form onSubmit={hnadleSubmit}>
-      <input type="text" name="name" placeholder='name' id="" />
-      <br />
-      <input type="email" name="email" placeholder='email' id="" />
-      <br />
-      <input type="submit" value="Add user" />
+        <input type="text" name="name" placeholder='name' id="" />
+        <br />
+        <input type="email" name="email" placeholder='email' id="" />
+        <br />
+        <input type="submit" value="Add user" />
       </form>
       {
         users.map(user => <p key={user.id}>{user.id}:{user.name}</p>)
